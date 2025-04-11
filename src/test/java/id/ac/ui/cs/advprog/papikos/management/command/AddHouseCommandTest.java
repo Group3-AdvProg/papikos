09b3083ae67b046e1e8fce7d8858881c877069fc
thataruby <thata@dfi.id>
2025-04-11 17:04:15 +0700

    [RED] Add tests for Add House Command

---
+++ src/test/java/id/ac/ui/cs/advprog/papikos/management/command/AddHouseCommandTest.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.papikos.management.command;
+
+import id.ac.ui.cs.advprog.papikos.management.model.House;
+import id.ac.ui.cs.advprog.papikos.management.service.HouseManagementService;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+class AddHouseCommandTest {
+
+    @Test
+    void callAddHouseInService() {
+        HouseManagementService mockService = mock(HouseManagementService.class);
+        House house = new House("Kos A", "Jakarta", "Desc", 3, 1500000.0);
+        AddHouseCommand command = new AddHouseCommand(mockService, house);
+
+        command.execute();
+
+        verify(mockService).addHouse(house);
+    }
+}
