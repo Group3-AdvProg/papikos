9835025f6da4c21d2dd0156d0bf8e0b93c620ad7
thataruby <thata@dfi.id>
2025-04-11 16:12:25 +0700

    [RED] Add tests for HouseRepository

---
+++ src/test/java/id/ac/ui/cs/advprog/papikos/management/repository/HouseRepositoryTest.java
@@ -0,0 +1,45 @@
+package id.ac.ui.cs.advprog.papikos.management.repository;
+
+import id.ac.ui.cs.advprog.papikos.management.model.House;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class HouseRepositoryTest {
+
+    @Autowired
+    private HouseRepository houseRepository;
+
+    private House house;
+
+    @BeforeEach
+    void setUp() {
+        house = new House("Kos Harapan", "Depok", "Bersih dan aman", 4, 1300000.0);
+        houseRepository.save(house);
+    }
+
+    @Test
+    void testSaveAndFindById() {
+        Optional<House> found = houseRepository.findById(house.getId());
+        assertTrue(found.isPresent());
+        assertEquals(house.getName(), found.get().getName());
+    }
+
+    @Test
+    void testFindAll() {
+        assertEquals(1, houseRepository.findAll().size());
+    }
+
+    @Test
+    void testDelete() {
+        houseRepository.deleteById(house.getId());
+        Optional<House> found = houseRepository.findById(house.getId());
+        assertFalse(found.isPresent());
+    }
+}
