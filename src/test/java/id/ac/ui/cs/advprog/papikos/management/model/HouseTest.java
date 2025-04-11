18330369a869fbce68f5fef00186fa4d7a92e75e
thataruby <thata@dfi.id>
2025-04-11 15:13:44 +0700

    [RED] Add tests for House model

---
+++ src/test/java/id/ac/ui/cs/advprog/papikos/management/model/HouseTest.java
@@ -0,0 +1,33 @@
+package id.ac.ui.cs.advprog.papikos.management.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HouseTest {
+
+    @Test
+    void testCreateHouseSuccess() {
+        House house = new House("Kos Harapan", "Depok", "Nyaman", 5, 1200000.0);
+        assertEquals("Kos Harapan", house.getName());
+        assertEquals("Depok", house.getAddress());
+        assertEquals("Nyaman", house.getDescription());
+        assertEquals(5, house.getNumberOfRooms());
+        assertEquals(1200000.0, house.getMonthlyRent());
+    }
+
+    @Test
+    void testSettersAndGetters() {
+        House house = new House();
+        house.setName("Kos A");
+        house.setAddress("Jakarta");
+        house.setDescription("Bersih");
+        house.setNumberOfRooms(3);
+        house.setMonthlyRent(950000.0);
+
+        assertEquals("Kos A", house.getName());
+        assertEquals("Jakarta", house.getAddress());
+        assertEquals("Bersih", house.getDescription());
+        assertEquals(3, house.getNumberOfRooms());
+        assertEquals(950000.0, house.getMonthlyRent());
+    }
+}
