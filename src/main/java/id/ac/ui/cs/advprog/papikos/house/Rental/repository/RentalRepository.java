bbf05e80cdd1eb2336bf625716bce3f3e006d693
DarrenM07 <darrenms7120@gmail.com>
2025-05-08 13:32:05 +0700

    [RED] Edit Repository to use UUID

--- src/main/java/id/ac/ui/cs/advprog/papikos/house/Rental/repository/RentalRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/papikos/house/Rental/repository/RentalRepository.java
@@ -2,6 +2,11 @@ package id.ac.ui.cs.advprog.papikos.house.Rental.repository;
 
 import id.ac.ui.cs.advprog.papikos.house.Rental.model.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface RentalRepository extends JpaRepository<Rental, Long> {
+import java.util.UUID;
+
+@Repository
+public interface RentalRepository extends JpaRepository<Rental, UUID> {
+    // sekarang UUID, bukan Long
 }
--- src/main/java/id/ac/ui/cs/advprog/papikos/house/Rental/repository/TenantRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/papikos/house/Rental/repository/TenantRepository.java
@@ -2,6 +2,11 @@ package id.ac.ui.cs.advprog.papikos.house.Rental.repository;
 
 import id.ac.ui.cs.advprog.papikos.house.Rental.model.Tenant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface TenantRepository extends JpaRepository<Tenant, Long> {
+import java.util.UUID;
+
+@Repository
+public interface TenantRepository extends JpaRepository<Tenant, UUID> {
+    // juga pakai UUID
 }
