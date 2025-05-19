500329766060d41557e7902d9db57fcb248e9735
sugonamu <je0jwff@gmail.com>
2025-05-19 10:55:53 +0100

    branch from staging

--- src/main/java/id/ac/ui/cs/advprog/papikos/wishlist/observer/NotificationObserver.java
+++
@@ -1,5 +0,0 @@
-package id.ac.ui.cs.advprog.papikos.wishlist.observer;
-
-public interface NotificationObserver {
-    void update(String roomType);
-}
--- src/main/java/id/ac/ui/cs/advprog/papikos/wishlist/observer/WishlistNotifier.java
+++
@@ -1,7 +0,0 @@
-package id.ac.ui.cs.advprog.papikos.wishlist.observer;
-
-public interface WishlistNotifier {
-    void registerObserver(String roomType, NotificationObserver observer);
-    void removeObserver(String roomType, NotificationObserver observer);
-    void notifyObservers(String roomType);
-}
