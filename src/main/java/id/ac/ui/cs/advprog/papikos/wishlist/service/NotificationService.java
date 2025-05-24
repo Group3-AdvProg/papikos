534162944a9ecb5879f276e456b06c3c3ece3e9a
thataruby <thata@dfi.id>
2025-05-25 05:59:37 +0700

    [RED] Implemented reject rental requests & notify rejection

--- src/main/java/id/ac/ui/cs/advprog/papikos/wishlist/service/NotificationService.java
+++ src/main/java/id/ac/ui/cs/advprog/papikos/wishlist/service/NotificationService.java
@@ -9,5 +9,6 @@ public interface NotificationService {
     List<Notification> getNotificationsBySender(Long senderId);
     CompletableFuture<Void> notifyAvailability(Long houseId);
     void sendToAllUsers(Long senderId, String message);
-    void notifyTenantRentalApproved(Long senderId, Long receiverId, Long houseId);
+    CompletableFuture<Void> notifyTenantRentalApproved(Long senderId, Long receiverId, Long houseId);
+    CompletableFuture<Void> notifyTenantRentalRejected(Long senderId, Long receiverId, Long houseId);
 }
